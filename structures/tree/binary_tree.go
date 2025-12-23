ec6c19ce955724d846fc2609631b6a8ac5b91276
Jose Rodriguez <4713082+josephrodriguez@users.noreply.github.com>
2025-12-23 15:16:50 +0200

    Implement Add method for binary tree

--- structures/tree/binary_tree.go
+++ structures/tree/binary_tree.go
@@ -2,12 +2,45 @@ package tree
 
 import "cmp"
 
-type BinaryNode[T cmp.Ordered] struct {
+type node[T cmp.Ordered] struct {
 	value T
-	left  *BinaryNode[T]
-	right *BinaryNode[T]
+	left  *node[T]
+	right *node[T]
 }
 
 type BinaryTree[T cmp.Ordered] struct {
-	root *BinaryNode[T]
+	root *node[T]
+}
+
+func NewBinaryTree[T cmp.Ordered]() *BinaryTree[T] {
+	return &BinaryTree[T]{
+		root: nil,
+	}
+}
+
+func (t *BinaryTree[T]) Add(element T) {
+
+	if t.root == nil {
+		t.root = &node[T]{value: element}
+		return
+	}
+
+	for current := t.root; ; {
+		switch {
+		case element > current.value:
+			if current.right == nil {
+				current.right = &node[T]{value: element}
+				return
+			}
+			current = current.right
+		case element < current.value:
+			if current.left == nil {
+				current.left = &node[T]{value: element}
+				return
+			}
+			current = current.left
+		case element == current.value:
+			return
+		}
+	}
 }
